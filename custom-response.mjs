62265876277920e942fd280242aadf244a3430f4
GDC-Reis <gustavodonisete11@gmail.com>
2026-01-12 21:46:37 -0300

    :construction: Commit adicionado com comentários no código

--- custom-response.mjs
+++ custom-response.mjs
@@ -1,14 +1,19 @@
-export function customResponse (res) {
+// Essa função estende o objeto res (response) adicionando dois métodos
+
+export function customResponse(res) {
+  // Cria um método chamado status (Criação de método dentro do res)
   res.status = (statusCode) => {
     res.statusCode = statusCode;
     return res;
-  }
+  };
+
+  // Cria um método chamado status
   res.json = value => {
-    try{
+    try {
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(value));
     }
-    catch{
+    catch {
       res.status(500).end("Erro");
     }
   };
